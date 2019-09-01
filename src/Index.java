9882c5b592c8c36e3b72f487fa2b63330d535e8d
Dmitry Kozhinov <d.kozhinov@mail.ru>
2019-09-01 10:51:47 +0500

    Add method doMethod2 in class Index.java

--- src/Index.java
+++ src/Index.java
@@ -1,5 +1,13 @@
 public class Index {
     public static void main(String args[]) {
         System.out.println("Hello, world!");
+        doMethod2(6);
     }
+
+    private static void doMethod2(int n) {
+    	for(int i=0; i<n; i++) {
+    		System.out.println("item#" + (i+1));
+    	}
+    }
+
 }
