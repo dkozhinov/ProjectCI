01a92a9399f856c13250294c180c8ff78f38fb73
Dmitry Kozhinov <d.kozhinov@mail.ru>
2019-09-01 10:43:00 +0500

    Add method doMethod in class Index.java

--- src/Index.java
+++ src/Index.java
@@ -1,5 +1,12 @@
 public class Index {
     public static void main(String args[]) {
         System.out.println("Hello, world!");
+
+        doMethod("Checking method!");
     }
+
+	private static String doMethod(String str) {
+		return str;
+	}
+
 }
