3e952417c5e6a1f8220b4f2bb9afb86a3d6224ed
Dmitry Kozhinov <d.kozhinov@mail.ru>
2019-09-01 11:19:53 +0500

    Create method doMethod2 in Index.java

--- src/Index.java
+++ src/Index.java
@@ -3,10 +3,17 @@ public class Index {
         System.out.println("Hello, world!");
 
         doMethod("Checking method!");
+        doMethod2(6);
     }
 
 	private static String doMethod(String str) {
 		return str;
+	}
+
+	private static void doMethod2(int n) {
+		for(int i=0; i<n; i++) {
+			System.out.println("item#" + (i+1));
+		}
 	}
 
 }
